a1d3bfdf9dfb11538f12b3b8865b5fbe05f8ce83
attawayinc <65624093+attawayinc@users.noreply.github.com>
2021-11-02 18:25:35 +0000

    .devcontainer

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/workspaces/devops/.venv"
+set -gx VIRTUAL_ENV "/workspaces/openemr-devops/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
